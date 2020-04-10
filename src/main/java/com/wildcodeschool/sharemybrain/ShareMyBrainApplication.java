e14f16c2edb81b6a39a00e7a8f218f26a662b126
bastienwcs <bastien@wldcodeschool.fr>
2020-04-10 08:38:02 +0200

    init

---
+++ src/main/java/com/wildcodeschool/sharemybrain/ShareMyBrainApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.sharemybrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareMyBrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareMyBrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/sharemybrain/ShareMyBrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.sharemybrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareMyBrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

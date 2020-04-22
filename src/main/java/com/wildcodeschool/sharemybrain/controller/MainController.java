0c401c35620f312aaa40032189f38016cf6489ab
mroldan747 <mroldan747@gmail.com>
2020-04-22 21:24:49 +0200

    questions.html working, without the data base for questions

--- src/main/java/com/wildcodeschool/sharemybrain/controller/MainController.java
+++ src/main/java/com/wildcodeschool/sharemybrain/controller/MainController.java
@@ -3,6 +3,7 @@ package com.wildcodeschool.sharemybrain.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -10,7 +11,7 @@ public class MainController {
     @GetMapping("/")
     public String index(Model model) {
         Boolean logged;
-        logged = true;
+        logged = false;
         model.addAttribute("logged", logged);
         return "index";
     }

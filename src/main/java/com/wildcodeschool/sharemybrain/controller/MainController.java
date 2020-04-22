5526d52464a417967704a1f104f34c31ab4b86cb
mroldan747 <mroldan747@gmail.com>
2020-04-22 10:54:47 +0200

    MainController is working. header is working on mobile for register and login

--- src/main/java/com/wildcodeschool/sharemybrain/controller/MainController.java
+++ src/main/java/com/wildcodeschool/sharemybrain/controller/MainController.java
@@ -3,7 +3,6 @@ package com.wildcodeschool.sharemybrain.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
@@ -11,21 +10,8 @@ public class MainController {
     @GetMapping("/")
     public String index(Model model) {
         Boolean logged;
-        logged = false;
+        logged = true;
         model.addAttribute("logged", logged);
         return "index";
     }
-
-    @GetMapping("/login")
-    public String login() {
-
-        return "login";
-    }
-
-    @GetMapping("/register")
-    public String register() {
-
-        return "register";
-    }
-
 }

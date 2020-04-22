15eab73a3cda07d9f7a446a7b55993f7711fb36b
Aurélien Lonni <aurelien.lonni@gmail.com>
2020-04-22 10:34:30 +0200

    UserController again

--- src/main/java/com/wildcodeschool/sharemybrain/controller/UserController.java
+++ src/main/java/com/wildcodeschool/sharemybrain/controller/UserController.java
@@ -1,4 +1,21 @@
-package controller;
+package com.wildcodeschool.sharemybrain.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+
+    @GetMapping("/register")
+    public String showRegisterPage() {
+
+        return "/register.html";
+    }
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+
+        return "/login.html";
+    }
+
 }

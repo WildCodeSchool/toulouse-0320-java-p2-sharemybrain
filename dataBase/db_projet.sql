ea43f6bdbe21362677fc1747c3211f39cffd84ff
mroldan747 <mroldan747@gmail.com>
2020-04-28 11:28:02 +0200

    modification of size of password

--- dataBase/db_projet.sql
+++ dataBase/db_projet.sql
@@ -33,7 +33,7 @@ CREATE TABLE user(
         id_user   Int  Auto_increment  NOT NULL ,
         username Varchar (50) NOT NULL ,
         email     Varchar (50) NOT NULL ,
-        password  Varchar (256) NOT NULL ,
+        password  Varchar (255) NOT NULL ,
         id_avatar Int NOT NULL ,
         id_skill  Int
 	,CONSTRAINT user_PK PRIMARY KEY (id_user)
